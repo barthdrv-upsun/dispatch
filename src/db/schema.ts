58a364c84f1b13c82a789192befd19263f8ef5cd
Mariel Okonkwo <mariel.okonkwo@pacenote.dev>
2025-06-02 10:06:56 +0200

    fix(db): tighten the schema barrel

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -2,3 +2,4 @@ export * from './schema/squads.js';
 export * from './schema/athletes.js';
 export * from './schema/sessions.js';
 export * from './schema/goals.js';
+export * from './schema/strava.js';
